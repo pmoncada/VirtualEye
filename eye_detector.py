34f422f6ce3df17246618d9dc6b8e7970efab4d5
Cyrus Nikolaidis <cyni@Cyruss-MacBook-Pro.local>
2017-01-02 20:00:19 -0500

    eye detector

---
+++ eye_detector.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+# Using a pretrained model, this function computes the location of a face and the eyes on the face,
+# in order to generate 8 point correspondences between images
+def eye_detector(img):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    eye_array = []
+    for index, (x,y,w,h) in enumerate(faces):
+        if(index): continue
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            eye_array.append([ex,ey,ew,eh])
+    return eye_array
+
+# Given the left bottom corner point, width and height of a rectangle
+# Returns the four corner points
+def point_reconstruct(point):
+    x,y,w,h = point[0],point[1],point[2],point[3]
+    point0 = [x, y]
+    point1 = [x + w, y]
+    point2 = [x, y + h]
+    point3 = [x + w, y + h]
+    return [point0, point1, point2, point3]
+
+# Returns two arrays of point correspondences between the two images
+def point_corresponder(eye_array_1, eye_array_2):
+    sorted(eye_array_1,key = lambda entry:entry[0])
+    sorted(eye_array_2,key = lambda entry:entry[0])
+
+    first_img = point_reconstruct(eye_array_1[0]) + point_reconstruct(eye_array_1[1])
+    second_img = point_reconstruct(eye_array_2[0]) + point_reconstruct(eye_array_2[1])
+    return first_img, second_img
+
+
+
+    
